6a756345aa2e3d416a488bdea413c19450976e80
Daler Asrorov <asrorids@gmail.com>
2016-09-26 02:36:03 -0700

    some progress in UI

--- Lab3/Lab3/ViewController.m
+++ Lab3/Lab3/ViewController.m
@@ -16,6 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _tipValue.adjustsFontSizeToFitWidth = NO;
+    _tipValue.lineBreakMode = NSLineBreakByTruncatingTail;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
