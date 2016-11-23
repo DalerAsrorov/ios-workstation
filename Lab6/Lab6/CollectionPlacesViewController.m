cd80725ce0d4d6382822d336cb4db961ddadf782
Daler Asrorov <asrorids@gmail.com>
2016-11-23 06:08:39 -0800

    uploaded all images, all webviews are working

--- Lab6/Lab6/CollectionPlacesViewController.m
+++ Lab6/Lab6/CollectionPlacesViewController.m
@@ -23,7 +23,7 @@ static NSString * const reuseIdentifier = @"PlaceCell";
 - (void)viewDidLoad {
     [super viewDidLoad];
     _model = [Model sharedModel];
-    
+    self.navigationItem.title = @"Tour Around the World";
     
     // Uncomment the following line to preserve selection between presentations
     // self.clearsSelectionOnViewWillAppear = NO;
