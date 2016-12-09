e214649587cb6de3981e5b0bad0c9fdaf783b338
Daler Asrorov <asrorids@gmail.com>
2016-12-09 02:15:48 -0800

    implemented the whole Add view controller functionalities and styles

--- Capnote/Capnote/CreateViewController.h
+++ Capnote/Capnote/CreateViewController.h
@@ -8,18 +8,29 @@
 
 #import <UIKit/UIKit.h>
 #import <Firebase/Firebase.h>
+#import "NotesModel.h"
 
-@interface CreateViewController : UIViewController<UIPickerViewDataSource, UIPickerViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@interface CreateViewController : UIViewController<UIPickerViewDataSource, UIPickerViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate, UITextViewDelegate, UIAlertViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextView *desc;
 @property (weak, nonatomic) IBOutlet UIButton *addImageButton;
-@property (weak, nonatomic) IBOutlet UITextView *textView;
 @property (weak, nonatomic) IBOutlet UIView *imagesBox;
+
+@property (weak, nonatomic) IBOutlet UITextField *titleTextField;
+
 @property (weak, nonatomic) IBOutlet UIPickerView *sectionPicker;
+
+@property (weak) UIAlertController * alert;
 
 @property UIImagePickerController *selectedImagePicker;
 
 - (void) styleManually;
+- (void) addNewPhotoToList: (UIImage *) chosenImage;
+- (void) addNote: (NSMutableArray *) photoListPointers;
+- (void) storePhotosToGoogleStorage;
+- (void) hideKeyboards;
+- (void) showMessage: (NSString*)message withTitle:(NSString *)title;
+- (void) clearAll;
 
 - (IBAction)addNewImage:(id)sender;
 - (IBAction)submitNote:(id)sender;
--- Capnote/Capnote/CreateViewController.m
+++ Capnote/Capnote/CreateViewController.m
@@ -11,6 +11,9 @@
 @interface CreateViewController ()
 
 @property NSArray *pickerData;
+@property NSMutableArray *photoList;
+@property NSMutableArray *photoListPointer;
+@property NotesModel *notesModel;
 
 @end
 
@@ -20,6 +23,7 @@ static int defaultHeight;
 static int dynamicX;
 static int dynamicY;
 static int xNextLocationBuffer;
+static NSString* tempUsername = @"dalerasrorov";
 
 @implementation CreateViewController
 
@@ -32,8 +36,20 @@ static int xNextLocationBuffer;
     // Connect data
     self.sectionPicker.dataSource = self;
     self.sectionPicker.delegate = self;
+   
+    // initialize vars
     self.pickerData = @[@"Item 1", @"Item 2", @"Item 3", @"Item 4", @"Item 5", @"Item 6"];
+    self.photoList = [NSMutableArray new];
+    self.photoListPointer = [NSMutableArray new];
+    self.notesModel = [[NotesModel alloc] init];
     
+
+    // text stuff delegates
+    [self.desc setDelegate: self];
+    [self.titleTextField setDelegate: self];
+    
+    // picker initialize
+    [self.sectionPicker selectRow:4 inComponent:0 animated:YES];
     
     // initializing static variables
     buttnCounter = 0;
@@ -45,16 +61,55 @@ static int xNextLocationBuffer;
     xNextLocationBuffer = 10;
 }
 
+- (void) clearAll {
+    self.desc.text = @"";
+    self.titleTextField.text = @"";
+    
+    [self.sectionPicker selectRow:4 inComponent:0 animated:YES];
+    
+    // removes images inside of the image view box
+    NSArray *viewsToRemove = [self.imagesBox subviews];
+    
+    for (UIView *v in viewsToRemove) {
+        [v removeFromSuperview];
+    }
+    
+    
+    
+}
+
+- (void)showMessage: (NSString*) message withTitle:(NSString *)title {
+    // @"Created Note!"
+    
+    // initialize success message box controller
+    UIAlertController *alert = [UIAlertController
+                  alertControllerWithTitle:@"Created note!"
+                  message:message
+                  preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction* okButton = [UIAlertAction
+                               actionWithTitle: title
+                               style: UIAlertActionStyleDefault
+                               handler:^(UIAlertAction * action) {
+                                   [self clearAll];
+                               }];
+    
+    
+    [alert addAction:okButton];
+    [self presentViewController: alert animated:YES completion:nil];
+}
+
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
 - (void) styleManually {
-    self.textView.layer.cornerRadius = 5.0;
-    self.textView.clipsToBounds = YES;
-    [self.textView.layer setBorderColor:[[[UIColor grayColor] colorWithAlphaComponent:0.5] CGColor]];
-    [self.textView.layer setBorderWidth:2.0];
+    self.desc.layer.cornerRadius = 5.0;
+    self.desc.clipsToBounds = YES;
+    [self.desc.layer setBorderColor:[[[UIColor grayColor] colorWithAlphaComponent:0.5] CGColor]];
+    [self.desc.layer setBorderWidth: 1.0];
     
 }
 
@@ -69,21 +124,105 @@ static int xNextLocationBuffer;
      self.selectedImagePicker.allowsEditing = YES;
      self.selectedImagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
     
-    [self presentViewController: self.selectedImagePicker animated:YES completion:NULL];
-    
+    [self presentViewController: self.selectedImagePicker animated:YES completion:NULL];   
+}
 
+- (void) addNote : (NSMutableArray *) photoListPointers {
+    NSInteger index;
+    NSString *noteSection;
+    NSString *noteTitle = self.titleTextField.text;
+    NSString *noteDescription = self.desc.text;
+    NSString *noteAdder = tempUsername;
+    
+    index = [self.sectionPicker selectedRowInComponent:0];
+    noteSection = [self.pickerData objectAtIndex: index];
+    
+    NSLog(@"photoList pointer: %@", self.photoListPointer);
+    
+    NSDictionary *postDict = @{@"adder" : noteAdder,
+                           @"description" : noteDescription,
+                           @"files" : photoListPointers,
+                           @"section" : noteSection,
+                           @"title" : noteTitle
+                           };
+    
+    [self.notesModel addNote:postDict];
+    
+}
+
+- (void) hideKeyboards {
+    [self.titleTextField resignFirstResponder];
+    [self.desc resignFirstResponder];
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [[self view] endEditing:TRUE];
+}
+
+- (void) storePhotosToGoogleStorage {
+    unsigned int photoCounter = 0;
+    
+    for (UIImage* image in self.photoList) {
+        NSData *imageData = UIImagePNGRepresentation(image);
+        NSString *fileName = [image accessibilityIdentifier];
+        NSString *imageKeyName = [self.notesModel generateKey];
+        NSString* photoCounterStr = [NSString stringWithFormat:@"%i", photoCounter];
+        NSString *imageKeyNameStr = [NSString stringWithFormat:@"images/%@-%@.png", imageKeyName, photoCounterStr];
+        
+        
+        // create reference to storage
+        FIRStorage *storage = [FIRStorage storage];
+        
+        // Create a storage reference from our storage service
+        FIRStorageReference *storageRef = [storage referenceForURL:@"gs://capnote-36deb.appspot.com"];
+        
+        // Create a reference to the file you want to upload
+        FIRStorageReference *imagesRef = [storageRef child:imageKeyNameStr];
+        
+        FIRStorageUploadTask *uploadTask = [imagesRef putData:imageData metadata:nil completion:^(FIRStorageMetadata *metadata, NSError *error) {
+            if (error != nil) {
+                // Uh-oh, an error occurred!
+                NSLog(@"Could not upload file %@", error);
+            } else {
+                [self showMessage:@"New note has been created." withTitle: @"OK"];
+                // Metadata contains file metadata such as size, content-type, and download URL.
+                NSURL *downloadURL = metadata.downloadURL;
+                NSString *absolutePath = downloadURL.absoluteString;
+                
+                NSArray *subviews = self.imagesBox.subviews;
+                [self.photoListPointer addObject: absolutePath];
+                
+                // if all images are uploaded, do the following
+                if(subviews.count == self.photoListPointer.count) {
+                    [self addNote : self.photoListPointer];
+                }
+                
+            }
+            
+        }];
+        
+        photoCounter++;
+        // Upload the file to the path "images/rivers.jpg"
+    }
     
 }
 
 - (IBAction)submitNote:(id)sender {
+    NSLog(@"List: %@", self.photoList);
+
+    // stored pointers to URLs in self.photoListPointer
+    [self storePhotosToGoogleStorage];
     
-    
+//    // construct a dict and add note
+//    [self addNote];
 }
-
-
 
 - (void) imagePickerControllerDidCancel:(UIImagePickerController *) picker {
     [self.selectedImagePicker dismissViewControllerAnimated:YES completion:NULL];
+}
+
+- (void) addNewPhotoToList: (UIImage *) chosenImage {
+    [self.photoList addObject: chosenImage];
 }
 
 - (void)imagePickerController:(UIImagePickerController *) thePicker didFinishPickingMediaWithInfo:(NSDictionary *)info {
@@ -95,36 +234,10 @@ static int xNextLocationBuffer;
     [self.imagesBox addSubview: customImageView];
     [self.selectedImagePicker dismissViewControllerAnimated:YES completion:NULL];
     
+    [self addNewPhotoToList : chosenImage];
+    
 
-    
-    
-    NSData *imageData = UIImagePNGRepresentation(chosenImage);
-    NSString *fileName = [chosenImage accessibilityIdentifier];
-    NSString *fullPath = [NSString stringWithFormat:@"%@/%@", imageData, fileName];
-    
-    // create reference to storage
-    FIRStorage *storage = [FIRStorage storage];
-    
-    // Create a storage reference from our storage service
-    FIRStorageReference *storageRef = [storage referenceForURL:@"gs://capnote-36deb.appspot.com"];
-    
-    // Create a reference to the file you want to upload
-    FIRStorageReference *imagesRef = [storageRef child:@"image.png"];
-    
-    // Upload the file to the path "images/rivers.jpg"
-    
-    FIRStorageUploadTask *uploadTask = [imagesRef putData:imageData metadata:nil completion:^(FIRStorageMetadata *metadata, NSError *error) {
-        if (error != nil) {
-            // Uh-oh, an error occurred!
-            NSLog(@"Uploaded file succesfully");
-        } else {
-            // Metadata contains file metadata such as size, content-type, and download URL.
-            NSURL *downloadURL = metadata.downloadURL;
-            NSLog(@"Downloads url %@", downloadURL);
-        }
-    }];
-    
-    
+    //
     
     // increament cyrrent X coordinate for the next picked image
     dynamicX += defaultWidth + xNextLocationBuffer;
@@ -148,6 +261,7 @@ static int xNextLocationBuffer;
 {
     return self.pickerData[row];
 }
+
 
 
 /*
